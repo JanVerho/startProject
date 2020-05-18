bbd2fd28809eed96acc8f42ad0b2214ffb69ca60
JanVerhoeven <zoete.inval.bevel@gmail.com>
2020-05-19 00:42:53 +0200

    Add  to class Filter a Default Ctor and a Ctor with param Productlist Add List <Products> prop to stock db_context_products

--- startProject/Logic/Filter.cs
+++ startProject/Logic/Filter.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using startProject.Model;
 
 namespace startProject.Logic
 {
     public class Filter
     {
+        public List<Product> ProductList { get; set; }
+
+        public Filter()
+        {
+        }
+
+        public Filter(List<Product> productList)
+        {
+            ProductList = productList;
+        }
     }
 }
