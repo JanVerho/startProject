3d80bfe30d25a88fa826ebe0c3b5570238bc4673
JanVerhoeven <zoete.inval.bevel@gmail.com>
2020-05-27 00:34:39 +0200

    Create Class OrderLine

---
+++ startProject/Model/OrderLine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace startProject.Model
+{
+    public class OrderLine
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Aantal")]
+        [Range(1, int.MaxValue, ErrorMessage = "'{0}' moet groter of gelijk zijn aan {1} .")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "'{0}' is verplicht.")]
+        public int Quantity { get; set; }
+
+        [ForeignKey("ProductId")]
+        [Range(0, 10, ErrorMessage = "'{0' waarde is niet in lijst")]
+        public Product Product { get; set; }
+
+        // constructors
+        public OrderLine()
+        {
+        }
+
+        public OrderLine(int quantityOrdered)
+        {
+            this.Quantity = quantityOrdered;
+        }
+
+        public OrderLine(Product product, int orderQuantity)
+        {
+            this.Product = product;
+            this.Quantity = orderQuantity;
+        }
+    }
+}
