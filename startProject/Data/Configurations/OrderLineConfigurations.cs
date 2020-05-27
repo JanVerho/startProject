3abb10999dbb043ddffdb79fd05744197ae5cd34
JanVerhoeven <zoete.inval.bevel@gmail.com>
2020-05-27 23:27:55 +0200

    Create Config OrderLine

---
+++ startProject/Data/Configurations/OrderLineConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using startProject.Model;
+
+namespace startProject.Data.Configurations
+{
+    public class OrderLineConfigurations : IEntityTypeConfiguration<OrderLine>
+    {
+        public void Configure(EntityTypeBuilder<OrderLine> builder)
+        {
+        }
+    }
+}
