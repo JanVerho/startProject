06bfd16c80fcd3427e195944e3e05cd12523231e
JanVerhoeven <zoete.inval.bevel@gmail.com>
2020-05-30 00:14:30 +0200

    ReUsed code from Master for Product valdation and IndexPage

--- startProject/Model/Product.cs
+++ startProject/Model/Product.cs
@@ -6,15 +6,19 @@ namespace startProject.Model
     public class Product
     {
         [Key]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "'{0}' is verplicht.")]
         public int Id { get; set; }
 
         [DisplayName("Naam")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "'{0}' is verplicht.")]
         public string Name { get; set; }
 
         [DisplayName("Bloei vanaf")]
+        [Range(1, 52, ErrorMessage = "'{0}' moet min {1} en max {2} zijn .")]
         public int WeekNrFlowerStart { get; set; }
 
         [DisplayName("Bloei tot")]
+        [Range(1, 52, ErrorMessage = "'{0}' moet min {1} en max {2} zijn .")]
         public int WeekNrFlowerEnd { get; set; }
 
         public Product()
