06e98003edacb1fffd67b0656e544bc3d1e9dc0e
JanVerhoeven <zoete.inval.bevel@gmail.com>
2020-05-31 22:45:14 +0200

    Change default for "is valid number" in validation-script

--- startProject/wwwroot/lib/jquery-validation/dist/jquery.validate.js
+++ startProject/wwwroot/lib/jquery-validation/dist/jquery.validate.js
@@ -355,7 +355,7 @@
             url: "Please enter a valid URL.",
             date: "Please enter a valid date.",
             dateISO: "Please enter a valid date (ISO).",
-            number: "Please enter a valid number.",
+            number: "Dit is geen geldig nummer.",
             digits: "Please enter only digits.",
             equalTo: "Please enter the same value again.",
             maxlength: $.validator.format("Please enter no more than {0} characters."),
