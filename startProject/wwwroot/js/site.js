745587cbb284db75fbfb8b708444d0db43b288bd
JanVerhoeven <zoete.inval.bevel@gmail.com>
2020-05-31 16:00:25 +0200

    Create JQ-filter in site.js, and debug

--- startProject/wwwroot/js/site.js
+++ startProject/wwwroot/js/site.js
@@ -2,3 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(document).ready(function () {
+    jQuery.fn.filterByText = function (textbox, selectSingleMatch) {
+        return this.each(function () {
+            var select = this;
+            var options = [];
+            $(select).find('option').each(function () {
+                let option = {
+                    value: $(this).val(),
+                    text: $(this).text()
+                };
+                options.push(option);
+            });
+            $(select).data('options', options);
+
+            $(textbox).bind('change keyup', function () {
+                $(select).empty();
+                $(select).scrollTop(0);
+                var options = $(select).data('options');;
+                var search = $.trim($(this).val());
+                var regex = new RegExp(search, 'gi');
+
+                $.each(options, function (i) {
+                    var option = options[i];
+                    if (option.text.match(regex) !== null) {
+                        $(select).append(
+                            $('<option>').text(option.text).val(option.value)
+                        );
+                    }
+                });
+                if (selectSingleMatch === true &&
+                    $(select).children().length === 1) {
+                    $(select).children().get(0).selected = true;
+                }
+                $(select).attr('size', 5);
+            });
+        });
+    };
+
+    $('#OrderLine_ProductName').filterByText($('#OrderLine_DZIMenuItemId_textbox'), true);
+});
+
+    //http://www.lessanvaezi.com/filter-select-list-options/
