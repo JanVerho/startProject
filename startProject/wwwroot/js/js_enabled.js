3ffc0968f53033ad68098f9e102af541a95bd8ca
JanVerhoeven <zoete.inval.bevel@gmail.com>
2020-06-03 23:06:48 +0200

    Moved "noScript" as a script to js folder in file js_enabled.js Refactored script - removed css in site.css Implemented css as an attribute in script

---
+++ startProject/wwwroot/js/js_enabled.js
@@ -0,0 +1,8 @@
+﻿// check and show on index-page if browser has JS enabled
+
+$(document).ready(function () {
+    let confirmText = "JavaScript: ingeschakeld!";
+
+    $("#JSTest").text(confirmText);
+    $("#JSTest").css({ "color": "green", "font-weight": "normal", "font-style": "italic" });
+});
