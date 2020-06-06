8e8fb0fea259e11344172c7048a61c6d14056dc1
JanVerhoeven <zoete.inval.bevel@gmail.com>
2020-06-06 23:03:56 +0200

    Refacto colorPickerScript -add code out of main - replace with methods

--- startProject/wwwroot/js/colorPickerScript.js
+++ startProject/wwwroot/js/colorPickerScript.js
@@ -1,27 +1,43 @@
-﻿$(document).ready(function () {
-    //Pimp change color with ColorPickerr
+﻿// startProject colorPickerScript.js
+
+$(document).ready(function () {
+    //Pimp change color with ColorPicker
+    checkJSIsEnabled();
+    setColorFromCookie();
+
+    if ($('#colorpicker') && $('#color')) {
+        pickAndSetNewColor();
+        setBodyBackGroundColor();
+    };
+});
+
+function checkJSIsEnabled() {
     if ($("#demo")) {
         $("#demo").hide();
     }
+}
 
-    if ($('#colorpicker') && $('#color')) {
-        $('#colorpicker').farbtastic('#color');
-        $('#colorpicker').on("click", function () {
+function setColorFromCookie() {
+    $("#color").attr('value', (Cookies.get('ColorPicked_Body')));
+}
+
+function pickAndSetNewColor() {
+    $('#colorpicker').farbtastic('#color');
+}
+
+function setBodyBackGroundColor() {
+    $('#colorpicker').on("click", function () {
+        if (confirmFunction($("#color").val())) {
+            $("body").css("background-color", $("#color").val());
             Cookies.set('ColorPicked_Body', $("#color").val(), { expires: 7, path: '' });
-            if (confirmFunction()) {
-                $("body").css("background-color", $("#color").val());
-                alert(Cookies.get('ColorPicked_Body'));
-            }
-        });
-    }
+            $("#color").attr('value', (Cookies.get('ColorPicked_Body')));
+        }
+    });
+}
 
-    Cookies.set('ColorPicked_Body', $("#color").val(), { expires: 7, path: '' });
-    alert(Cookies.get('ColorPicked_Body'));
-});
-
-function confirmFunction() {
+function confirmFunction(newColorCode) {
     var txt;
-    var r = confirm("Achtergrond wijzigen naar de gekozen kleur?");
+    var r = confirm("Achtergrond wijzigen naar de gekozen kleur(" + newColorCode + ")?");
     if (r == true) {
         txt = "You pressed OK!";
     } else {
